dbbef7827dbdb7f051f693451eacd5312d243099
Huzaifa <huzaifaiftikhar671@gmail.com>
2022-08-27 18:22:35 +0500

    change 1

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php include 'header.php' ?>
 <div class="container">
     <div class="row">
-        <div class="col-lg-4 mx-auto">
+        <div class="col-lg-4 mx-auto text-center">
             <h1>Hello World</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad, consequatur distinctio dolorem
+                doloremque dolorum eveniet exercitationem impedit, molestiae molestias nam officiis optio quia sed,
+                suscipit? A possimus praesentium vel?</p>
+            <button class="btn btn-lg btn-danger text-uppercase">stay away</button>
         </div>
     </div>
 </div>
