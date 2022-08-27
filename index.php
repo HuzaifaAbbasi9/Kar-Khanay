e85d5f435bd27761ff0fe4650a64c2e25c85a03b
Huzaifa <huzaifaiftikhar671@gmail.com>
2022-08-27 17:59:33 +0500

    changes 123

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
     <div class="row">
         <div class="col-lg-4 mx-auto">
             <h1>Hello World</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto commodi dolorem ducimus hic minima
+                minus nulla praesentium quidem repellat, voluptate. Aliquid aspernatur at eos iste iusto libero pariatur
+                perferendis reprehenderit.</p>
         </div>
     </div>
 </div>
