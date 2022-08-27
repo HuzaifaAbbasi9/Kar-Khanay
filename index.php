5af295f02616eaded092bd44d4ea03adad2f5aa0
Huzaifa <huzaifaiftikhar671@gmail.com>
2022-08-27 17:40:24 +0500

    changes

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include 'header.php' ?>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4 mx-auto">
+            <h1>Hello World</h1>
+        </div>
+    </div>
+</div>
+<?php include 'footer.php' ?>
+
