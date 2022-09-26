d9fab51e4d22697ff6610fa59df664ccaef0e887
huzaifaAbb <huzaifaiftikhar671@gmail.com>
2022-09-26 13:01:37 +0500

    aksda

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,38 @@
 $(document).ready(function () {
-
+    $('.hero_slider').owlCarousel({
+        items: 1, dots: true, loop: true, nav: false, responsive: {
+            0: {
+                margin: 0
+            }, 992: {
+                margin: 1
+            }
+        }
+    })
+    $('.navbar-toggler').click(function (e) {
+        $(this).parents('.navbar').toggleClass('open');
+    });
+    $('.footer-heading').click(function (e) {
+        e.preventDefault();
+        let media = window.matchMedia('(max-width: 991px)').matches;
+        if (media) {
+            $(this).toggleClass('active').next('ul').fadeToggle();
+        }
+    });
+    $('.headerMini_cart').click(function (e) {
+        e.preventDefault();
+        $(this).toggleClass('openCart');
+        let miniCart = $('.header-mini-cart');
+        miniCart.fadeToggle(300);
+    });
+    $(document).click(function (e) {
+        let target = $(e.target);
+        if (target.parents('.headerMini_cart').length === 0 && target.parents('.header-mini-cart').length === 0) {
+            let miniCart = $('.header-mini-cart');
+            miniCart.fadeOut(300).removeClass('openCart');
+        }
+        // let miniCart = $('.header-mini-cart');
+        // if (target.parents('.headerMini_cart').hasClass('openCart')) {
+        //     miniCart.fadeOut(300);
+        // }
+    });
 });
--- footer.php
+++ footer.php
@@ -1,6 +1,139 @@
+<footer class="custom-container">
+    <div class="container-fluid p-0">
+        <div class="row">
+            <div class="col-lg-2 footer-item">
+                <h5 class="footer-heading">Categories</h5>
+                <ul>
+                    <li><a href="#"><h5>Women Fashion</h5></a></li>
+                    <li><a href="#"><h5>Mobile Phones</h5></a></li>
+                    <li><a href="#"><h5>Kids & Toys</h5></a></li>
+                    <li><a href="#"><h5>Men Fashion</h5></a></li>
+                    <li><a href="#"><h5>Baby Dresses</h5></a></li>
+                    <li><a href="#"><h5>Make up</h5></a></li>
+                    <li><a href="#"><h5>Watches</h5></a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 footer-item">
+                <h5 class="footer-heading">About</h5>
+                <ul>
+                    <li><a href="#"><h5>Adress: <span>St no,Road no,area,country</span></h5></a></li>
+                    <li><a href="#"><h5>Phone: <span>0300-1234567</span></h5></a></li>
+                    <li><a href="#"><h5>Email: <span class="footer-email-link">demo.example@gmail.com</span></h5></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-2 footer-item">
+                <h5 class="footer-heading">Support</h5>
+                <ul>
+                    <li><a href="#"><h5>Help</h5></a></li>
+                    <li><a href="#"><h5>Support Us</h5></a></li>
+                    <li><a href="#"><h5>Contact Us</h5></a></li>
+                    <li><a href="#"><h5>Join Us</h5></a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 footer-item">
+                <h5 class="footer-heading">Community</h5>
+                <ul>
+                    <li><a href="#"><h5>Community</h5></a></li>
+                    <li><a href="#"><h5>Community</h5></a></li>
+                    <li><a href="#"><h5>Community</h5></a></li>
+                    <li><a href="#"><h5>Community</h5></a></li>
+                    <li><a href="#"><h5>Community</h5></a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 footer-item">
+                <img src="images/footer-logo.png" class="footer-logo" alt="">
+                <form class="subscribe_form">
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="email" placeholder="Enter your email">
+                        <span class="input-group-btn">
+                    <button class="btn btn-default" type="button">Subscribe</button>
+               </span>
+                    </div>
+                </form>
+                <div class="footer-become-seller">
+                    <h4>Become a Seller</h4>
+                    <a href="#" class="main_btn">Apply Now</a>
+                </div>
+            </div>
+        </div>
+        <div class="row footer-bottom">
+            <div class="col-lg-5 d-flex align-items-end">
+                <img src="images/footer-small.png" class="footer-small-icon" alt="">
+                <p class="footer-copyright main_para">&copy; Karkhanay International Ltd. 2022</p>
+            </div>
+            <div class="col-lg-5 d-flex align-items-end">
+                <a href="#" class="footer_icon">
+                    <svg id="instagram" xmlns="http://www.w3.org/2000/svg" width="28.359" height="28.359"
+                         viewBox="0 0 28.359 28.359">
+                        <path id="XML-ID_17_"
+                              d="M19.911,0H8.448A8.458,8.458,0,0,0,0,8.448V19.911a8.458,8.458,0,0,0,8.448,8.448H19.911a8.458,8.458,0,0,0,8.448-8.448V8.448A8.458,8.458,0,0,0,19.911,0Zm5.6,19.911a5.6,5.6,0,0,1-5.6,5.6H8.448a5.6,5.6,0,0,1-5.6-5.6V8.448a5.6,5.6,0,0,1,5.6-5.6H19.911a5.6,5.6,0,0,1,5.6,5.6V19.911Z"
+                              fill="currentColor"/>
+                        <path id="XML-ID_81_"
+                              d="M140.335,133a7.335,7.335,0,1,0,7.335,7.335A7.343,7.343,0,0,0,140.335,133Zm0,11.817a4.482,4.482,0,1,1,4.482-4.482A4.482,4.482,0,0,1,140.335,144.817Z"
+                              transform="translate(-126.155 -126.155)" fill="currentColor"/>
+                        <ellipse id="XML-ID_83_" cx="1.758" cy="1.758" rx="1.758" ry="1.758"
+                                 transform="translate(19.771 5.143)" fill="currentColor"/>
+                    </svg>
+                </a>
+                <a href="#" class="footer_icon">
+                    <svg id="youtube" xmlns="http://www.w3.org/2000/svg" width="40.077" height="28.359"
+                         viewBox="0 0 40.077 28.359">
+                        <path id="Path_22" data-name="Path 22"
+                              d="M31.753,67.393H8.323A8.323,8.323,0,0,0,0,75.716V87.429a8.323,8.323,0,0,0,8.323,8.323h23.43a8.323,8.323,0,0,0,8.323-8.323V75.716A8.323,8.323,0,0,0,31.753,67.393Zm-5.629,14.75L15.165,87.369a.44.44,0,0,1-.629-.4V76.192a.44.44,0,0,1,.639-.392l10.959,5.553A.44.44,0,0,1,26.124,82.143Z"
+                              transform="translate(0 -67.393)" fill="currentColor"/>
+                    </svg>
+                </a>
+                <a href="#" class="footer_icon">
+                    <svg id="linkedin" xmlns="http://www.w3.org/2000/svg" width="28.359" height="28.359"
+                         viewBox="0 0 28.359 28.359">
+                        <path id="Path_20" data-name="Path 20" d="M245.986,203.726v-.041c-.008.014-.02.028-.028.041Z"
+                              transform="translate(-230.628 -190.99)" fill="currentColor" fill-rule="evenodd"/>
+                        <path id="Path_21" data-name="Path 21"
+                              d="M0,0V28.359H28.359V0ZM8.821,23.56H4.613V10.9H8.821ZM6.717,9.174H6.689A2.193,2.193,0,1,1,6.745,4.8a2.194,2.194,0,1,1-.028,4.375ZM24,23.56H19.788V16.788c0-1.7-.608-2.862-2.131-2.862A2.3,2.3,0,0,0,15.5,15.465a2.884,2.884,0,0,0-.14,1.026v7.07H11.148s.056-11.471,0-12.659h4.209V12.7A4.18,4.18,0,0,1,19.151,10.6C21.92,10.6,24,12.413,24,16.3Z"
+                              fill="currentColor" fill-rule="evenodd"/>
+                    </svg>
+
+                </a>
+                <a href="#" class="footer_icon">
+                    <svg id="facebook" xmlns="http://www.w3.org/2000/svg" width="30.078" height="30.078"
+                         viewBox="0 0 30.078 30.078">
+                        <path id="Path_18" data-name="Path 18"
+                              d="M15.039,0A15.039,15.039,0,1,1,0,15.039,15.039,15.039,0,0,1,15.039,0Z"
+                              fill="currentColor"/>
+                        <path id="Path_19" data-name="Path 19"
+                              d="M114.738,75.41h1.938V72.547H114.4v.01c-2.761.1-3.326,1.65-3.376,3.279h-.006v1.43h-1.88v2.8h1.88v7.516h2.833V80.07h2.321l.448-2.8H113.85V76.4A.923.923,0,0,1,114.738,75.41Z"
+                              transform="translate(-97.868 -65.057)" fill="#ffffff"/>
+                    </svg>
+                </a>
+                <a href="#" class="footer_icon">
+                    <svg id="twitter" xmlns="http://www.w3.org/2000/svg" width="34.629" height="28.352"
+                         viewBox="0 0 34.629 28.352">
+                        <path id="XML-ID_827_"
+                              d="M33.842,31.369a13.126,13.126,0,0,1-1.675.61,7.4,7.4,0,0,0,1.507-2.651.559.559,0,0,0-.817-.651,13.181,13.181,0,0,1-3.9,1.54,7.437,7.437,0,0,0-12.637,5.313,7.583,7.583,0,0,0,.061.96,19.032,19.032,0,0,1-13.063-6.93.559.559,0,0,0-.916.072,7.445,7.445,0,0,0,.762,8.556,6.3,6.3,0,0,1-.995-.444.559.559,0,0,0-.83.476c0,.033,0,.066,0,.1a7.46,7.46,0,0,0,3.639,6.393c-.189-.019-.378-.046-.566-.082a.559.559,0,0,0-.637.719,7.431,7.431,0,0,0,5.445,4.983A13.151,13.151,0,0,1,2.2,52.338a13.449,13.449,0,0,1-1.574-.092.559.559,0,0,0-.367,1.025A20.047,20.047,0,0,0,11.1,56.448a19.286,19.286,0,0,0,14.942-6.563,20.8,20.8,0,0,0,5.178-13.557c0-.2,0-.41-.009-.615a14.451,14.451,0,0,0,3.326-3.523.559.559,0,0,0-.691-.821Z"
+                              transform="translate(0.002 -28.097)" fill="currentColor"/>
+                    </svg>
+
+                </a>
+            </div>
+            <div class="col-lg-2 d-flex align-items-end footer-language">
+                <svg xmlns="http://www.w3.org/2000/svg" width="23.513" height="23.513" viewBox="0 0 23.513 23.513">
+                    <path id="globe"
+                          d="M0,11.756A11.756,11.756,0,1,1,11.756,23.513,11.756,11.756,0,0,1,0,11.756ZM11.022,1.583A5.171,5.171,0,0,0,8.249,4.309a11.712,11.712,0,0,0-.688,1.569h3.461v-4.3ZM6.01,5.878a13.618,13.618,0,0,1,.941-2.262,9.846,9.846,0,0,1,.877-1.371A10.324,10.324,0,0,0,3.314,5.878Zm-.855,5.143a20.136,20.136,0,0,1,.458-3.674H2.46A10.225,10.225,0,0,0,1.5,11.022ZM7.123,7.348a18.369,18.369,0,0,0-.5,3.674h4.4V7.348Zm5.368,0v3.674h4.394a18.362,18.362,0,0,0-.5-3.674ZM6.628,12.491a18.369,18.369,0,0,0,.5,3.674h3.9V12.491Zm5.863,0v3.674h3.9a18.3,18.3,0,0,0,.5-3.674Zm-4.93,5.143A11.7,11.7,0,0,0,8.249,19.2a5.181,5.181,0,0,0,2.773,2.726v-4.3Zm.267,3.633A9.84,9.84,0,0,1,6.951,19.9a13.62,13.62,0,0,1-.941-2.262h-2.7a10.322,10.322,0,0,0,4.514,3.633Zm-2.215-5.1a20.062,20.062,0,0,1-.458-3.674H1.5a10.178,10.178,0,0,0,.964,3.674Zm10.071,5.1A10.322,10.322,0,0,0,20.2,17.635H17.5a13.622,13.622,0,0,1-.941,2.262A9.828,9.828,0,0,1,15.684,21.267Zm-3.193-3.633v4.3A5.171,5.171,0,0,0,15.264,19.2a11.7,11.7,0,0,0,.688-1.569Zm5.408-1.47h3.154a10.179,10.179,0,0,0,.964-3.674H18.358A20.059,20.059,0,0,1,17.9,16.165Zm4.118-5.143a10.226,10.226,0,0,0-.964-3.674H17.9a20.137,20.137,0,0,1,.458,3.674ZM16.562,3.617A13.6,13.6,0,0,1,17.5,5.878h2.7a10.322,10.322,0,0,0-4.514-3.633,9.975,9.975,0,0,1,.877,1.371Zm-.61,2.262a11.707,11.707,0,0,0-.688-1.569,5.176,5.176,0,0,0-2.773-2.726v4.3Z"
+                          fill="#74767e"/>
+                </svg>
+                <span class="main_para">
+                    English
+                </span>
+            </div>
+        </div>
+    </div>
+</footer>
+
 <script src=" assets/js/jquery.min.js"></script>
 <script src="assets/js/popper.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
 <script src="assets/js/script.js"></script>
 </body>
 </html>
